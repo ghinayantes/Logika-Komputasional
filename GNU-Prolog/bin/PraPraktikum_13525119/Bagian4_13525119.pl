dfca2af103e6e0fb9ca55074c9789c67f9ef72c4
fromdearest <ghinayantes2006@gmail.com>
2026-05-03 10:23:38 +0700

    Fix format

--- GNU-Prolog/bin/PraPraktikum_13525119/Bagian4_13525119.pl
+++ GNU-Prolog/bin/PraPraktikum_13525119/Bagian4_13525119.pl
@@ -6,6 +6,7 @@
 :-include('random_13525119.pl').
 
 /* start */
+/* Deklarasi Rules */
 start:- 
     (game_started -> write('Permainan sudah dimulai. Gunakan "exit" untuk keluar dan memulai ulang.'), nl
     ; 
@@ -16,12 +17,14 @@ start:-
     format('Kartumu: ~w~n', [Cards])).
 
 /* readCards */
+/* Deklarasi Rules Helper*/
 readCards(Cards):-
     open('kartu_13525119.txt', read, Stream),
     read(Stream, Cards),
     close(Stream).
 
 /* play_card */
+/* Deklarasi Rules */
 play_card:-
     (\+ game_started -> fail ; hand(H),
     (H = [] -> write('Kartumu sudah habis!'), nl ;
@@ -33,6 +36,7 @@ play_card:-
     (NewHand = [] -> write('Selamat! Kamu menghabiskan semua kartumu. Kamu menang!!!'), nl ; true))).
 
 /* display_status */
+/* Deklarasi Rules */
 display_status:-
     (\+ game_started -> fail
     ;
@@ -42,6 +46,7 @@ display_status:-
     format('Kartumu: ~w~n', [H])).
 
 /* exit */
+/* Deklarasi Rules */
 exit:-
     (\+ game_started -> write('Permainan belum dimulai. Gunakan "start" untuk memulai.'), nl
     ;
@@ -51,6 +56,7 @@ exit:-
     write('Sampai jumpa di meja kartu berikutnya.'), nl).
 
 /* writeResult */
+/* Deklarasi Rules Helper*/
 writeResult(FinalHand):-
     open('hasil_13525119.txt', write, Stream),
     (FinalHand = [] -> write(Stream, 'Status: Menang'), nl(Stream) ;  write(Stream, 'Status: Tidak Selesai'), nl(Stream)),
